529a6d203dd0e1f152da9b5f7fd8ef182ba36bdb
Mathieu Debove <debovema@users.noreply.github.com>
2017-12-01 14:45:35 +0100

    Improved dependencies management

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/debovema/go-api/handlers/content"
-	"github.com/debovema/go-api/handlers/middleware"
+	"./handlers/content"
+	"./handlers/middleware"
 	"os"
 	"io"
 )
